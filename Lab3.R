78f144ff1899c4850a415f38e66f279445853da4
will <williams.alvarez@usach.cl>
2020-08-17 21:19:12 -0400

    Creación lab

---
+++ Lab3.R
@@ -0,0 +1,8 @@
+library("ggpubr")
+library("arulesViz")
+library("dplyr")
+
+#Se cargan los datos
+datos <- read.csv('C:/Users/Will/Desktop/U/Analisis/Lab 3/bank-additional.csv', sep = ";")
+#datos <- read.csv('C:/bank-additional.csv', sep = ";")
+
