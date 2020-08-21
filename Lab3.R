6a63d5487482b5e202acec7c296ad7ee405a171d
Vicente <vicente.brevis@usach.cl>
2020-08-21 04:02:49 -0400

    Preparación conjunto de prueba
    Se quitan campos en blanco o desconocidos

--- Lab3.R
+++ Lab3.R
@@ -6,8 +6,13 @@ library("dplyr")
 #datos <- read.csv('C:/Users/Will/Desktop/U/Analisis/Lab 3/bank-additional.csv', sep = ";")
 datos <- read.csv('C:/bank-additional.csv', sep = ";")
 
+
+#Se borran registros con datos desconocidos del conjunto de prueba
+datos.dis <- select(datos, -y)
+datos.dis <- datos.dis[apply(datos.dis!="unknown", 1, all),] %>% droplevels()
+
+
 #Se discretizan las variables cuantitativas
-datos.dis <- datos
 datos.dis[,"age"] <- cut(datos.dis$age, breaks = c(15, 29, 59, 100), labels = c("young", "adult", "elderly"))
 datos.dis[,"duration"] <- cut(datos.dis$duration, breaks = c(-1, 60, 180, 480, 600, 4000), labels = c("very.short", "short", "average", "long", "very.long"))
 datos.dis[,"campaign"] <- cut(datos.dis$campaign, breaks = c(-1, 8, 16, 24, 32, 40), labels = c("less.than.eight", "nine.to.sixteen", "seventeen.to.twentyfour", "twentyfive.to.thrirtytwo", "more.than.thrirtythree"))
